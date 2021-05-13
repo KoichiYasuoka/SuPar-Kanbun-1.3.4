d97a4ee19ebbc28a822ebb291a6cf8fc4c97d726
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2021-05-13 15:34:26 +0900

    version 1.1.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ URL="https://github.com/KoichiYasuoka/SuPar-Kanbun"
 
 setuptools.setup(
   name="suparkanbun",
-  version="1.1.5",
+  version="1.1.6",
   description="Tokenizer POS-tagger and Dependency-parser for Classical Chinese",
   long_description=long_description,
   long_description_content_type="text/markdown",
@@ -19,7 +19,7 @@ setuptools.setup(
     "supar>=1.1.0",
     "transformers>=4.0.1",
     "spacy>=2.2.2",
-    "deplacy>=1.9.7"
+    "deplacy>=1.9.8"
   ],
   python_requires=">=3.7",
   package_data={"suparkanbun":["models/*.txt","models/*/*.txt","models/*/*.json"]},
